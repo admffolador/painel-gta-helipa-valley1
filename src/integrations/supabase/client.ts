0ec0350ec49609f65616c1ceb510e49bbfb59ee4
admffolador <admffolador@gmail.com>
2025-01-14 15:43:50 -0300

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://gtsgvyjtoucimewhtcqf.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0c2d2eWp0b3VjaW1ld2h0Y3FmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY2ODg4NDksImV4cCI6MjA1MjI2NDg0OX0.z6RNuQGPsEfyK4EVL7JihQo-VjYjP2E1dAFANtH_eiw";
+const SUPABASE_URL = "https://crofntqtplmapqlqwcze.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNyb2ZudHF0cGxtYXBxbHF3Y3plIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY4MjUwNDIsImV4cCI6MjA1MjQwMTA0Mn0.QEAwPvGTNBw-Af6hS-TI0oGeFRoUFl2E385_4S9YMx8";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
